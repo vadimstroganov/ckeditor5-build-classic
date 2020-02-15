af62a032cac8495b58069202d5f327e0f433702e
Vadim Stroganov <vadimstroganov@gmail.com>
2020-02-15 14:01:37 +0300

    Set default language RU. Change heading h1-h6

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -79,14 +79,17 @@ ClassicEditor.defaultConfig = {
 			'redo'
 		]
 	},
-	image: {
-		toolbar: [
-			'imageStyle:full',
-			'imageStyle:side',
-			'|',
-			'imageTextAlternative'
-		]
-	},
+  heading: {
+    options: [
+      { model: 'paragraph', title: 'Абзац', class: 'ck-heading_paragraph' },
+      { model: 'heading1', view: 'h1', title: 'Заголовок 1', class: 'ck-heading_heading1' },
+      { model: 'heading2', view: 'h2', title: 'Заголовок 2', class: 'ck-heading_heading2' },
+      { model: 'heading3', view: 'h3', title: 'Заголовок 3', class: 'ck-heading_heading3' },
+      { model: 'heading4', view: 'h4', title: 'Заголовок 4', class: 'ck-heading_heading4' },
+      { model: 'heading5', view: 'h5', title: 'Заголовок 5', class: 'ck-heading_heading5' },
+      { model: 'heading6', view: 'h6', title: 'Заголовок 6', class: 'ck-heading_heading6' },
+    ]
+  },
 	table: {
 		contentToolbar: [
 			'tableColumn',
@@ -95,5 +98,5 @@ ClassicEditor.defaultConfig = {
 		]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
+	language: 'ru'
 };
